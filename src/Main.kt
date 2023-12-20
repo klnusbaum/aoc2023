7b06aa80f21a79a11c2613b46bd3db1e61bb76b4
Kurtis Nusbaum <kcommiter@gmail.com>
2023-12-20 10:27:35 -0800

    day 1 cleanup and formatting

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,4 @@
 fun main() {
     problems.dayone.part1()
+    problems.dayone.part2()
 }
--- src/problems/dayone/dayone.kt
+++ src/problems/dayone/dayone.kt
@@ -2,16 +2,50 @@ package problems.dayone
 
 import java.io.File
 
+fun part1() {
+    val calibrationValues = File("input/day1/calibration_values.txt")
+    val sum = calibrationValues.useLines { sumLines(it) }
+
+    println("Sum of values is $sum")
+}
+
+fun sumLines(lines: Sequence<String>) = lines.map { parseLine(it) }.sum()
+
 fun parseLine(input: String): Int {
     val nums = input.filter { c -> c in '0'..'9' }
     return "${nums.first()}${nums.last()}".toInt()
 }
 
-fun sumLines(lines: Sequence<String>) = lines.map { line -> parseLine(line) }.sum()
-
-fun part1() {
+fun part2() {
     val calibrationValues = File("input/day1/calibration_values.txt")
-    val sum = calibrationValues.useLines { lines -> sumLines(lines) }
+    val sum = calibrationValues.useLines { sumLinesV2(it) }
 
     println("Sum of values is $sum")
+}
+
+fun sumLinesV2(lines: Sequence<String>) = lines.map{ parseLineV2(it) }.sum()
+
+fun parseLineV2(input: String): Int {
+    val first = input.findAnyOf(numTargets)?.second?.toNumString()
+    val last = input.findLastAnyOf(numTargets)?.second?.toNumString()
+
+    return "$first$last".toInt()
+}
+
+val numTargets = listOf(
+    "1", "2", "3", "4", "5", "6", "7", "8", "9",
+    "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"
+)
+
+fun String.toNumString() = when(this) {
+    "one" -> "1"
+    "two" -> "2"
+    "three" -> "3"
+    "four" -> "4"
+    "five" -> "5"
+    "six" -> "6"
+    "seven" -> "7"
+    "eight" -> "8"
+    "nine" -> "9"
+    else -> this
 }
