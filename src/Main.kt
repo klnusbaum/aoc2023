b6ff13cf8d7c69d0b65ae76529d4d037a531188d
Kurtis Nusbaum <kcommiter@gmail.com>
2023-12-19 21:44:02 -0800

    day1 part 1

---
+++ src/Main.kt
@@ -0,0 +1,4 @@
+import problems.dayone.runDayOne
+fun main() {
+    runDayOne()
+}
---
+++ src/problems/dayone/dayone.kt
@@ -0,0 +1,17 @@
+package problems.dayone
+
+import java.io.File
+
+fun parseLine(input: String): Int {
+    val nums = input.filter { c -> c in '0'..'9' }
+    return "${nums.first()}${nums.last()}".toInt()
+}
+
+fun sumLines(lines: Sequence<String>) = lines.map { line -> parseLine(line) }.sum()
+
+fun runDayOne() {
+    val calibrationValues = File("input/day1/calibration_values.txt")
+    val sum = calibrationValues.useLines { lines -> sumLines(lines) }
+
+    println("Sum of values is $sum")
+}
